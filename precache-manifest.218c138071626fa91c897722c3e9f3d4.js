e3cfc88cc57aefb6d985e5376b5259aec2eefc38
jeevanantham123 <51525786+jeevanantham123@users.noreply.github.com>
2020-05-15 21:57:57 +0530

    Updates

---
+++ precache-manifest.218c138071626fa91c897722c3e9f3d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4937ed73e473b60b5d14f8fa4b3720",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "bcf8935fb41db5cbc714",
+    "url": "/react_todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8a19b77a581ec9d69d84",
+    "url": "/react_todo/static/css/main.fed2389a.chunk.css"
+  },
+  {
+    "revision": "bcf8935fb41db5cbc714",
+    "url": "/react_todo/static/js/2.79235c6c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_todo/static/js/2.79235c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a19b77a581ec9d69d84",
+    "url": "/react_todo/static/js/main.456ae4d8.chunk.js"
+  },
+  {
+    "revision": "8fdbfb445b3836515c7a",
+    "url": "/react_todo/static/js/runtime-main.a3b5b2c3.js"
+  }
+]);
